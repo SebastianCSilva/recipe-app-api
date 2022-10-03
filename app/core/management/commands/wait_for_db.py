38eb8464b10685bb91cd6095f1d9a455496487f1
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-10-02 23:41:31 -0300

    se crean comandos para testear la espera de la base de datos

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django comando para esperar por la base de datos disponible
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    #Django comando para esperar para base de datos
+
+
+    def handle(self, *args, **options):
+        pass
