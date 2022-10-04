4ee434da95671108e10b9af06c1480149e920b00
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2022-10-04 01:35:43 -0300

    Se testea y normaliza la entrada de emails

--- app/core/models.py
+++ app/core/models.py
@@ -10,8 +10,8 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extra_field):
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
